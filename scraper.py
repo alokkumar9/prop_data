2ab691eea5a83c88a7e3c72695f920e2b57a7475
alok <alokkumar991992@gmail.com>
2024-09-29 00:47:53 +0530

    afer .env add

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ client=connect_to_atlas()
 mg_db=client["property"]
 mg_collection=mg_db["maharashtra"]
 
-pages = [i for i in range(691,692)]  # Define the pages to scrape
+pages = [i for i in range(692,721)]  # Define the pages to scrape
 # all_projects_list = []
 driver=setup_driver()
 
